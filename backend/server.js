7f4743342481b229b097da01ebb6280ca1d9b071
ahmadayazuddin2023-source <ahmadayazuddin2023@gmail.com>
2026-05-07 19:13:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,4 @@ mongoose.connect(process.env.MONGO_URL)
 
 // server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0' => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
